56042143d253c0b0616c3d98cbd14415a22d0dde
Humberto Barbosa <humberto.obarbosa@gmail.com>
2019-10-31 08:33:32 -0300

    add eslint standard

--- backend/app/Models/User.js
+++ backend/app/Models/User.js
@@ -35,7 +35,7 @@ class User extends Model {
     return this.hasMany('App/Models/Token')
   }
 
-  teams() {
+  teams () {
     return this.belongsToMany('App/Models/Team')
       .pivotModel('App/Models/UserTeam')
   }
